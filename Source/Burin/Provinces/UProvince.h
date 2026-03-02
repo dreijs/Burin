afb135e2fc9cb51a025c56b600787c96c414b6df
dreijs <daniel.reijsbergen@gmail.com>
2026-03-02 21:48:34 +0800

    added map mode menu

---
+++ Source/Burin/Provinces/UProvince.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "UProvince.generated.h"
+
+UCLASS(Blueprintable)
+class BURIN_API UProvince : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UProvince();
+	~UProvince();
+
+};
---
+++ Source/Burin/UWorldCreator.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "UWorldCreator.generated.h"
+
+UCLASS(Blueprintable)
+class BURIN_API UWorldCreator : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UWorldCreator();
+	~UWorldCreator();
+
+};
